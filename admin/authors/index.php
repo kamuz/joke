bfd002c0acfbe352b168b8acfbb6123920e242f7
Vladimir Kamuz <v.kamuz@gmail.com>
2016-10-19 04:03:42 -0400

    Edit author

--- admin/authors/index.php
+++ admin/authors/index.php
@@ -19,20 +19,71 @@ if (isset($_GET['addform'])) {
   include $_SERVER['DOCUMENT_ROOT'] . '/includes/db.inc.php';
 
   try {
-    $sql = 'INSERT INTO author SET
-      name = :name,
-      email = :email';
+    $sql = 'INSERT INTO author SET name = :name, email = :email';
     $s = $pdo->prepare($sql);
     $s->bindValue(':name', $_POST['name']);
     $s->bindValue(':email', $_POST['email']);
     $s->execute();
   }
-  catch (Exception $e) {
+  catch (PDOException $e) {
     $error = 'Error adding submitted author.';
     include 'error.html.php';
     exit();
   }
   
+  header('Location: .');
+  exit();
+}
+
+// Edit author
+
+// show data
+if (isset($_POST['action']) && $_POST['action'] == 'Edit') {
+  include $_SERVER ['DOCUMENT_ROOT'] . '/includes/db.inc.php';
+
+  try {
+    $sql = 'SELECT id, name, email FROM author WHERE id = :id';
+    $s = $pdo->prepare($sql);
+    $s->bindValue(':id', $_POST['id']);
+    $s->execute();
+  }
+  catch(PDOExeption $e) {
+    $error = 'Error fetching author details.';
+    include 'error.html.php';
+    exit();
+  }
+
+  $row = $s->fetch();
+
+  $pageTitle = 'Edit Author';
+  $action = 'editform';
+  $name = $row['name'];
+  $email = $row['email'];
+  $id = $row['id'];
+  $button = 'Update author';
+
+  include 'form.html.php';
+  exit();
+}
+
+// save data
+if (isset($_GET['editform'])){
+  include $_SERVER['DOCUMENT_ROOT'] . '/includes/db.inc.php';
+
+  try {
+    $sql = 'UPDATE author SET name = :name, email = :email WHERE id = :id';
+    $s = $pdo->prepare($sql);
+    $s->bindValue(':name', $_POST['name']);
+    $s->bindValue(':email', $_POST['email']);
+    $s->bindValue(':id', $_POST['id']);
+    $s->execute();
+  }
+  catch(PDOExeption $e){
+    $error = 'Error updating submitted author.';
+    include 'error.html.php';
+    exit();
+  }
+
   header('Location: .');
   exit();
 }
@@ -43,7 +94,7 @@ if (isset($_POST['action']) && $_POST['action'] == 'Delete'){
 
   include $_SERVER['DOCUMENT_ROOT'] . '/includes/db.inc.php';
 
-  // Get ID jokes belonging to author
+  // get ID jokes belonging to author
   try{
     $sql = 'SELECT id FROM joke WHERE authorid = :id';
     $s = $pdo->prepare($sql);
@@ -58,7 +109,7 @@ if (isset($_POST['action']) && $_POST['action'] == 'Delete'){
 
   $result = $s->fetchAll();
 
-  // Delete joke category entries
+  // delete joke category entries
   try {
     $sql = 'DELETE FROM jokecategory WHERE jokeid = :id';
     $s = $pdo->prepare($sql);
@@ -76,7 +127,7 @@ if (isset($_POST['action']) && $_POST['action'] == 'Delete'){
     exit();
   }
 
-  // Delete jokes belonging to author
+  // delete jokes belonging to author
   try {
     $sql = 'DELETE FROM joke WHERE authorid = :id';
     $s = $pdo->prepare($sql);
@@ -89,7 +140,7 @@ if (isset($_POST['action']) && $_POST['action'] == 'Delete'){
     exit();
   }
 
-  // Delete the author
+  // delete the author
   try {
     $sql = 'DELETE FROM author WHERE id = :id';
     $s = $pdo->prepare($sql);
